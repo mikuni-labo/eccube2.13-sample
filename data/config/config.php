1479048776b9724d10607f6773766bb95b8619ca
Kuniyasu Wada <wada@n-di.co.jp>
2016-01-15 19:27:45 +0900

    初回コミット

---
+++ data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('HTTP_URL', 'http://eccube.local/');
+define('HTTPS_URL', 'http://eccube.local/');
+define('ROOT_URLPATH', '/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_SERVER', '127.0.0.1');
+define('DB_NAME', 'eccube_db');
+define('DB_PORT', '');
+define('ADMIN_DIR', 'admin/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'piamiauapruthenopobudearesoudratheacrouc');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'mail');
+define('SMTP_HOST', '');
+define('SMTP_PORT', '');
+define('SMTP_USER', '');
+define('SMTP_PASSWORD', '');
