3e037d04bbbac26941d3004eace3ba1363ce28a6
Kuniyasu_Wada <elephunk.com@icloud.com>
2017-03-19 14:17:33 +0900

    コミコミ

--- data/config/config.php
+++ data/config/config.php
@@ -1,15 +1,15 @@
 <?php
-define('ECCUBE_INSTALL', 'ON');
-define('HTTP_URL', 'http://eccube.local/');
-define('HTTPS_URL', 'http://eccube.local/');
+define('ECCUBE_INSTALL', 'OFF');
+define('HTTP_URL', 'http://127.0.0.1:8000/');
+define('HTTPS_URL', 'http://127.0.0.1:8000/');
 define('ROOT_URLPATH', '/');
 define('DOMAIN_NAME', '');
 define('DB_TYPE', 'mysql');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_SERVER', '127.0.0.1');
-define('DB_NAME', 'eccube_db');
-define('DB_PORT', '');
+define('DB_PASSWORD', 'kuniyasu1983');
+define('DB_SERVER', 'eccube2_mysql');
+define('DB_NAME', 'eccube2');
+define('DB_PORT', '3306');
 define('ADMIN_DIR', 'admin/');
 define('ADMIN_FORCE_SSL', FALSE);
 define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
@@ -20,3 +20,5 @@ define('SMTP_HOST', '');
 define('SMTP_PORT', '');
 define('SMTP_USER', '');
 define('SMTP_PASSWORD', '');
+
+ini_set('display_errors', 1);
